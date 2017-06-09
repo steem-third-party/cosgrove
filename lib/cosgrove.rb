acf067ded2251cc801c90e6eeb92d62c8a6be534
Anthony Martin <github@martin-studio.com>
2017-06-09 15:44:07 -0700

    config support

--- lib/cosgrove.rb
+++ lib/cosgrove.rb
@@ -1,4 +1,12 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'yaml'
+# require 'pry'
+
+Bundler.require
+
 module Cosgrove
   require 'cosgrove/version'
+  require 'cosgrove/config'
   require 'cosgrove/bot'
 end
---
+++ lib/cosgrove/config.rb
@@ -0,0 +1,71 @@
+module Cosgrove
+  module Config
+    def steem_api_url
+      chain[:steem_api_url]
+    end
+    
+    def golos_api_url
+      chain[:golos_api_url]
+    end
+    
+    def test_api_url
+      chain[:test_api_url]
+    end
+    
+    def steem_account
+      chain[:steem_account]
+    end
+    
+    def steem_posting_wif
+      chain[:steem_posting_wif]
+    end
+    
+    def golos_account
+      chain[:golos_account]
+    end
+    
+    def golos_posting_wif
+      chain[:golos_posting_wif]
+    end
+    
+    def test_posting_wif
+      chain[:test_posting_wif]
+    end
+    
+    def discord_channels
+      return ENV['CHANNELS'].to_s.split(' ') if !!ENV['CHANNELS']
+      
+      yml[:discord][:channels]
+    end
+    
+    def discord_log_mode
+      return :debug if !!ENV['HELL_ENABLED']
+      return :info unless !!yml[:discord][:log_mode]
+      
+      yml[:discord][:log_mode].to_sym
+    end
+    
+    def cosgrove_secure
+      yml[:cosgrove][:secure]
+    end
+  private
+    def chain
+      @chain ||= yml[:chain]
+    end
+    
+    def yml
+      @config_yaml_path ||= "config.yml"
+      @yml ||= if File.exist?(@config_yaml_path)
+        YAML.load_file(@config_yaml_path)
+      else
+        raise 'Create a file: config.yml'
+      end
+      
+      if @yml[:cosgrove].nil? || @yml[:cosgrove][:secure].nil? || @yml[:cosgrove][:secure] == 'set this'
+        raise 'Set secure key in config.yml.'
+      end
+      
+      @yml
+    end
+  end
+end
